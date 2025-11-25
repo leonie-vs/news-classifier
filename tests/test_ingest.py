348a28f15b93da4b9523bdb880596e27fd27037f
Leonie von Schirach <leonie.schirach@web.de>
2025-11-25 14:26:04 +0000

    test: add assert for train_data to test 1 to check datatype

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -4,4 +4,5 @@ from ingest import test_data, train_data
 # Test 1
 def test_train_and_test_data_is_read_in_as_dataframe():
     assert type(test_data) == pd.DataFrame
+    assert type(train_data) == pd.DataFrame
 
