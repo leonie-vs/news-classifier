0905f1ed62550712043cbae430a95c8cb0405ae6
Leonie von Schirach <leonie.schirach@web.de>
2025-11-25 14:48:22 +0000

    test: add test 2 to check each row has valid label 1-4

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -6,3 +6,12 @@ def test_train_and_test_data_is_read_in_as_dataframe():
     assert type(test_data) == pd.DataFrame
     assert type(train_data) == pd.DataFrame
 
+# Test 2
+def test_each_row_in_data_has_valid_label():
+    valid_labels = {1, 2, 3, 4}
+    assert test_data["label"].isin(valid_labels).all
+    assert train_data["label"].isin(valid_labels).all
+
+# Test 3
+def test_data_has_no_missing_values():
+
