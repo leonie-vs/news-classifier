bd206e3c3913970c266b892426742758aef2f480
Leonie von Schirach <leonie.schirach@web.de>
2025-11-26 09:06:40 +0000

    feat: add remove_special_characters function to clean title and description columns

--- ingest.py
+++ ingest.py
@@ -1,2 +1,21 @@
 import pandas as pd
+import re
 
+# load data
+test_data = pd.read_json(path_or_buf="data/test.jsonl", lines=True)
+train_data = pd.read_json(path_or_buf="data/train.jsonl", lines=True)
+
+# clean data
+def standardise_to_lower(df):
+    data = df.copy()
+    data['title'] = data['title'].str.lower()
+    data['description'] = data['description'].str.lower()
+    return data
+
+def remove_special_characters(df):
+    data = df.copy()
+    data['title'] = data['title'].str.replace(r'[^a-zA-Z0-9\s.,!?\':"-]', '', regex=True)
+    data['description'] = data['description'].str.replace(r'[^a-zA-Z0-9\s.,!?\':"-]', '', regex=True)
+    data['title'] = data['title'].str.replace(r'\s+', ' ', regex=True).str.strip()
+    data['description'] = data['description'].str.replace(r'\s+', ' ', regex=True).str.strip()
+    return data
