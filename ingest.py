90b8750bcb8a288fd956f35d2e78109193597e40
Leonie von Schirach <leonie.schirach@web.de>
2025-11-26 10:15:00 +0000

    feat: write cleaned data to new jsonl files

--- ingest.py
+++ ingest.py
@@ -8,15 +8,21 @@ train_data = pd.read_json(path_or_buf="data/train.jsonl", lines=True)
 # clean data
 # remove any characters that aren't a-z or basic punctuation 
 def remove_special_characters(df):
+    
     data = df.copy()
+    
     data['title'] = data['title'].str.replace(r'[^a-zA-Z0-9\s.,!?\':"-]', '', regex=True)
     data['description'] = data['description'].str.replace(r'[^a-zA-Z0-9\s.,!?\':"-]', '', regex=True)
+
     data['title'] = data['title'].str.replace(r'\s+', ' ', regex=True).str.strip()
     data['description'] = data['description'].str.replace(r'\s+', ' ', regex=True).str.strip()
+    
     return data
 
 # call cleaning function 
 test_cleaned = remove_special_characters(test_data)
 train_cleaned = remove_special_characters(train_data)
 
-# save cleaned datasets to new jsonl files
+# write cleaned data to new jsonl files
+test_cleaned.to_json('data/test_cleaned.jsonl', orient='records', lines=True)
+train_cleaned.to_json('data/train_cleaned.jsonl', orient='records', lines=True)
