fdf7eed64bc250d50937179d34d6c1ec7b503e5c
Leonie von Schirach <leonie.schirach@web.de>
2025-11-26 13:43:58 +0000

    feat: change script to hold IngestClass with methods for loading, cleaning, and saving data

--- ingest.py
+++ ingest.py
@@ -1,27 +1,38 @@
 import pandas as pd
 
-# define file path 
-path_data = "data/train.jsonl"
-
-# load data
-data_df = pd.read_json(path_or_buf=path_data, lines=True)
-
-# clean data function
-def remove_special_characters(df):
+class IngestData:
     
-    data = df.copy()
-    # remove any characters that aren't a-z or basic punctuation 
-    data['title'] = data['title'].str.replace(r'[^a-zA-Z0-9\s.,!?\':"-]', '', regex=True)
-    data['description'] = data['description'].str.replace(r'[^a-zA-Z0-9\s.,!?\':"-]', '', regex=True)
+    def __init__(self, input_path, output_path):
+        self.input_path = input_path
+        self.output_path = output_path
 
-    data['title'] = data['title'].str.replace(r'\s+', ' ', regex=True).str.strip()
-    data['description'] = data['description'].str.replace(r'\s+', ' ', regex=True).str.strip()
+    # load data
+    def load_data(self):
+        df = pd.read_json(path_or_buf=self.input_path, lines=True)
+        return df
+
+    # clean data 
+    def remove_special_characters(self, df):
+        data = df.copy()
+        # remove any characters that aren't a-z, A-Z, or basic punctuation 
+        data['title'] = data['title'].str.replace(r'[^a-zA-Z0-9\s.,!?\':"-]', '', regex=True)
+        data['description'] = data['description'].str.replace(r'[^a-zA-Z0-9\s.,!?\':"-]', '', regex=True)
+
+        data['title'] = data['title'].str.replace(r'\s+', ' ', regex=True).str.strip()
+        data['description'] = data['description'].str.replace(r'\s+', ' ', regex=True).str.strip()
+
+        return data
     
-    return data
+    # save data
+    def save_data(self, df):
+        df.to_json(self.output_path, orient='records', lines=True) # write data to jsonl file
 
-# preprocess and save data
-if __name__ == "__main__":
-    data_cleaned = remove_special_characters(data_df) # call cleaning function
-    data_cleaned.to_json('data/data_cleaned.jsonl', orient='records', lines=True) # write cleaned data to new jsonl file
-    print("Data cleaned and saved")
+    # load, preprocess, and save data in one method
+    def preprocess_pipeline(self):
+        df = self.load_data() # load raw data
+        data_cleaned = self.remove_special_characters(df) # call cleaning function
+        self.save_data(data_cleaned) # save cleaned data in output file
+        print("Data cleaned and saved")
+
+
 
