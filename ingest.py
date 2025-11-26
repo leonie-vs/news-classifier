a9d24c32aced4d1f024d8af7e3ae55ef51679f28
Leonie von Schirach <leonie.schirach@web.de>
2025-11-26 09:21:06 +0000

    feat: remove lowercase cleaning function

--- ingest.py
+++ ingest.py
@@ -6,12 +6,6 @@ test_data = pd.read_json(path_or_buf="data/test.jsonl", lines=True)
 train_data = pd.read_json(path_or_buf="data/train.jsonl", lines=True)
 
 # clean data
-# convert everything to lowercase in title and description
-def standardise_to_lower(df):
-    data = df.copy()
-    data['title'] = data['title'].str.lower()
-    data['description'] = data['description'].str.lower()
-    return data
 # remove any characters that aren't a-z or basic punctuation 
 def remove_special_characters(df):
     data = df.copy()
@@ -20,3 +14,9 @@ def remove_special_characters(df):
     data['title'] = data['title'].str.replace(r'\s+', ' ', regex=True).str.strip()
     data['description'] = data['description'].str.replace(r'\s+', ' ', regex=True).str.strip()
     return data
+
+# call cleaning function 
+test_cleaned = remove_special_characters(test_data)
+train_cleaned = remove_special_characters(train_data)
+
+# save cleaned datasets to new jsonl files
