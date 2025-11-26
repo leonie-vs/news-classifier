b8a6eab70047129219d137cad2ab603e715ff0bb
Leonie von Schirach <leonie.schirach@web.de>
2025-11-26 09:08:33 +0000

    style: update comments to improve readability

--- ingest.py
+++ ingest.py
@@ -6,12 +6,13 @@ test_data = pd.read_json(path_or_buf="data/test.jsonl", lines=True)
 train_data = pd.read_json(path_or_buf="data/train.jsonl", lines=True)
 
 # clean data
+# convert everything to lowercase in title and description
 def standardise_to_lower(df):
     data = df.copy()
     data['title'] = data['title'].str.lower()
     data['description'] = data['description'].str.lower()
     return data
-
+# remove any characters that aren't a-z or basic punctuation 
 def remove_special_characters(df):
     data = df.copy()
     data['title'] = data['title'].str.replace(r'[^a-zA-Z0-9\s.,!?\':"-]', '', regex=True)
