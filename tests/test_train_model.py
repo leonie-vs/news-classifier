4a72a7851a17f87496e06091315a02f1427604f4
Leonie von Schirach <leonie.schirach@web.de>
2025-11-26 15:16:41 +0000

    test: add test 1 for reduce_data method, including shape and unique labels assertions

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from train_model import TrainModel
-from ingest import IngestData
 
 # Test 1 
 def test_reduce_data_returns_balanced_dataframe():
     trainer = TrainModel()
-    d = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.DataFrame(data=d)
-    result = trainer.reduce_data(df,n=10)
+    df = pd.read_json(path_or_buf='./tests/test_data.jsonl', lines=True)
+    result = trainer.reduce_data(df, n=2)
     assert result['label'].nunique() == 4
-    assert result.shape() == (10,3)
+    assert result.shape == (8,3)
+    assert (result['label'].value_counts() == 2).all()
+
 
 
