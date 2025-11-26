432c5fc1f6c6ae9d3feb7117c70f6d6d58bdca17
Leonie von Schirach <leonie.schirach@web.de>
2025-11-26 08:54:05 +0000

    fix: minor changes to make sure tests check the right data

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from ingest import test_data, train_data
+from ingest import test_data, train_data, standardise_to_lower
 
 # Test 1
 def test_train_and_test_data_is_read_in_as_dataframe():
@@ -9,12 +9,20 @@ def test_train_and_test_data_is_read_in_as_dataframe():
 # Test 2
 def test_each_row_in_data_has_valid_label():
     valid_labels = {1, 2, 3, 4}
-    assert test_data["label"].isin(valid_labels).all
-    assert train_data["label"].isin(valid_labels).all
+    assert test_data["label"].isin(valid_labels).all()
+    assert train_data["label"].isin(valid_labels).all()
 
 # Test 3
 def test_data_has_no_missing_values():
     assert not test_data.isnull().any().any()
     assert not train_data.isnull().any().any()
+
+# Test 4
+def test_standardise_to_lower_converts_title_and_description_columns():
+    data = {'title': ['Hello', 'Bye'], 'description': ['oKaY', 'YES']}
+    test_df = pd.DataFrame(data) 
+    lower_df = standardise_to_lower(test_df)
+    assert lower_df['title'].tolist() == ['hello', 'bye']
+    assert lower_df['description'].tolist() == ['okay', 'yes']
 
 
