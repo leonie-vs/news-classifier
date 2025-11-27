a7608d7ef8aca57b982596dc826d85a735f5280a
Leonie von Schirach <leonie.schirach@web.de>
2025-11-27 14:21:58 +0000

    feat: refactor simple_interface script into one function

--- simple_interface.py
+++ simple_interface.py
@@ -1,20 +1,21 @@
 from news_classifier import NewsClassifier
 from ingest import IngestData 
 
-path = 'lr_news_model.pkl'
-news_clf = NewsClassifier(path)
+def simple_interface(path):
+    
+    news_clf = NewsClassifier(path)
+    
+    print("Hello, welcome to the News Classifier! Enter a news headline or type 'quit' to exit.\n")
 
-print("Hello, welcome to the News Classifier! Enter a news headline or type 'quit' to exit.\n")
+    while True:
 
-while True:
+        headline = input("> Enter a headline: ")
+        label = news_clf.classify_news(headline)
 
-    headline = input("> Enter a headline: ")
-    label = news_clf.classify_news(headline)
+        category = news_clf.get_category(label)
 
-    category = news_clf.get_category(label)
+        if headline == "quit":
+            print("\nGoodbye!")
+            break
 
-    if headline == "quit":
-        print("\nGoodbye!")
-        break
-
-    print(f"[Result] {category}\n")
+        print(f"[Result] {category}\n")
