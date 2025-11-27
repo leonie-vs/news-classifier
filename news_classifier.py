aa293e4154de456456488ae92e435832310fb6c1
Leonie von Schirach <leonie.schirach@web.de>
2025-11-27 13:39:25 +0000

    feat: add classify_news method to NewsClassifier class that returns predicted label

--- news_classifier.py
+++ news_classifier.py
@@ -7,8 +7,13 @@ class NewsClassifier:
         try:
             with open(self.path, 'rb') as f:
                 self.model = pickle.load(f)
-        except:
-            print("Model can't be found, please pass valid file path")
+        except Exception:
+            raise Exception("Model can't be found")
+    
+    def classify_news(self, headline):
+        pred = self.model.predict([headline])
+        return pred[0]
+
         
         
         
