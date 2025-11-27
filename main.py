216ef550f75bba313322ab1181315f745a53a28e
Leonie von Schirach <leonie.schirach@web.de>
2025-11-27 12:02:38 +0000

    chore: change import to train_lr_model after changing file name for more clarity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ingest import IngestData
-from train_model import TrainModel
+from train_lr_model import TrainModel
 import pandas as pd
 
 # define file path 
@@ -14,7 +14,7 @@ df = ingest.preprocess_pipeline()
 trainer = TrainModel()
 
 # reduce data to 50,000 rows
-train_reduced = trainer.reduce_data(df, n=12500)
+train_reduced = trainer.reduce_data(df)
 
 # split data into test and train
 trainer.split_data(train_reduced)
@@ -28,11 +28,24 @@ print(f"Accuracy: {accuracy:.4f}")
 print("\nClassification Report:")
 print(report)
 
-# predict example news headline
-headline = "Labour will not leave 'broken' welfare system unchanged, chancellor says"
-prediction = trainer.predict(headline)
-print(f"Predicted label: {prediction[0]}")
+# predict example news headlines
+# 1 - world
+headline1 = "Pope Leo arrives in Turkey on first foreign trip of papacy"
+prediction1 = trainer.predict(headline1)
+print(f"Prediction: {prediction1[0]}")
+# 2 - sports
+headline2 = "McLaren had 'porpoising concern' during Las Vegas GP"
+prediction2 = trainer.predict(headline2)
+print(f"Prediction: {prediction2[0]}")
+# 3 - business
+headline3 = "I earn £20,000 and live with my son. The Budget means we will pay more tax"
+prediction3 = trainer.predict(headline3)
+print(f"Prediction: {prediction3[0]}")
+# 4 - sci/tech
+headline4 = "'Extraordinary discovery' at Orkney Neolithic site"
+prediction4 = trainer.predict(headline4)
+print(f"Prediction: {prediction4[0]}")
 
 # save the model
-trainer.save_model()
+#trainer.save_model()
 
