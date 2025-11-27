70a47f4502eb0e719b640f91e353bbea7f4850dc
Leonie von Schirach <leonie.schirach@web.de>
2025-11-27 13:38:13 +0000

    test: add test 2 to check classify_news method returns correct label for news headline

--- tests/test_news_classifier.py
+++ tests/test_news_classifier.py
@@ -2,6 +2,16 @@ from news_classifier import NewsClassifier
 
 # Test 1 
 def test_news_classifier_path_attr_is_string():
-    path = 1
+    path = 'lr_news_model.pkl'
     clf = NewsClassifier(path)
-    assert clf.path == "1"
+    assert type(clf.path) == str
+    assert clf.path == 'lr_news_model.pkl'
+
+# Test 2 
+def test_classify_news_method_returns_label_prediction():
+    path = 'lr_news_model.pkl'
+    clf = NewsClassifier(path)
+    pred = clf.classify_news("Watch: Moment researcher finds rare flower after 13-year search")
+    assert pred == 4
+
+
