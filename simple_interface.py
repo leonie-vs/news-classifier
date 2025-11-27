f6b9c28623aa9db1533f217f95dfb7efc2cbafc8
Leonie von Schirach <leonie.schirach@web.de>
2025-11-27 14:14:58 +0000

    feat: refactor script by moving get_category function into NewsClassifier class

--- simple_interface.py
+++ simple_interface.py
@@ -1,26 +1,20 @@
 from news_classifier import NewsClassifier
+from ingest import IngestData 
 
 path = 'lr_news_model.pkl'
 news_clf = NewsClassifier(path)
 
-# define function to get category for each label
-def get_category(label):
-    label_mapping = {1: 'World', 2: 'Sports', 3: 'Business', 4: 'Sci/Tech'}
-    category = label_mapping[label]
-    return category
-
-print("Hello, welcome to the News Classifier!\n")
-print("Please type in a news headline you would like to have classified. Type 'quit' to exit.\n")
+print("Hello, welcome to the News Classifier! Enter a news headline or type 'quit' to exit.\n")
 
 while True:
 
-    headline = input("You: ")
+    headline = input("> Enter a headline: ")
     label = news_clf.classify_news(headline)
 
-    category = get_category(label)
+    category = news_clf.get_category(label)
 
     if headline == "quit":
         print("\nGoodbye!")
         break
 
-    print(f"Bot: This headline is in the category: {category}-news\n")
+    print(f"[Result] {category}\n")
